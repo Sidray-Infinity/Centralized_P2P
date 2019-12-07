353081b392403c0ed4cabb04cd39e1938594b57d
Siddharth Panda <siddharth.pandathedancer@gmail.com>
2019-12-07 21:13:59 +0530

    Can Chat noe

---
+++ client.c
@@ -0,0 +1,345 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+struct login {
+    int online;
+    char username[1024];
+    char password[1024];
+};
+
+struct peer {
+    int peer_id;
+    char ip[20];
+    int port;
+};
+
+void display_online_peers(struct peer *peer_list) {
+    printf("---------------------------------------------\n");
+    printf("PEERS ONLINE:\n");
+    for(int i=0; i<10; i++) {
+        if(peer_list[i].peer_id != 0)
+            printf("%d. PEER: %d IP: %s UDP_PORT: %d\n",i, peer_list[i].peer_id, peer_list[i].ip, peer_list[i].port);
+    }
+    printf("---------------------------------------------\n");
+}
+
+// If the server broadcasts the tcp socket and ip, how will the peers talk on a udp conenction?
+
+int isPeer(struct peer peerlist[], struct peer peer) {
+    // Determines if the new recieved peer is already in list or not.
+    
+    for(int i=0; i<9; i++)
+        if(strcmp(peerlist[i].ip, peer.ip) == 0 && peerlist[i].port == peer.port)
+            return 1;
+
+    return 0;
+}
+
+int isNumber(char *buff) {
+    // Determines if the string is a single digit.
+    buff[strlen(buff)-1] = '\0';
+    int num, flag = 0;
+    for(int j=0; j<strlen(buff); j++) {
+        if(buff[j] < '9' && buff[j] > '0')
+            flag = 1;
+        else {
+            flag = 0;
+            break;
+        }
+    }
+    if(flag == 1) {
+        sscanf(buff, "%d", &num);
+        return num;
+    }
+    return -1;
+}
+
+void chat(int sock_id, struct peer other) {
+    // A stupid char function
+
+    struct sockaddr_in other_struct, store; int len_store = sizeof(store);
+    other_struct.sin_family = AF_INET;
+    other_struct.sin_port = htons(other.port);
+    inet_pton(AF_INET, other.ip, &(other_struct.sin_addr));
+
+    char buff[1024]; 
+    int n; 
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+    while(1) { 
+        bzero(buff, 1024); 
+        printf("Enter a message to send:\n");
+        fgets(buff, 1024, stdin);
+
+        int send_id = sendto(sock_id, buff, strlen(buff), 0, (struct sockaddr*)&other_struct, sizeof(other_struct));
+        if(send_id == -1) {
+            printf("Cannot send the chat message!\n");
+            exit(1);
+        }
+        buff[strlen(buff)-1] = '\0';
+        if(strcmp(buff, "exit") == 0) {
+            printf("Quiting the chat session.\n");
+            break;
+        }
+
+        bzero(buff, 1024);
+        printf("Waiting for a reply...\n");
+
+        int recv_id = recvfrom(sock_id, buff, sizeof(buff),0 ,(struct sockaddr*)&store,&len_store);
+        if(recv_id == -1) {
+            printf("Cannot recieve the chat message:\n");
+            exit(1);
+        }
+
+        printf("MSG: %s\n", buff);
+        buff[strlen(buff)-1] = '\0';
+        if(strcmp(buff, "exit") == 0) {
+            printf("Quiting the chat session.\n");
+            break;
+        }
+    }
+    printf("---------------------------------------------\n"); 
+    printf("---------------------------------------------\n");
+}
+
+void chat_recv(int sock_id) {
+    // A stupid char function
+
+    struct sockaddr_in store; int len_store = sizeof(store);
+    
+    char buff[1024]; 
+    int n; 
+    printf("---------------------------------------------\n");
+    printf("---------------------------------------------\n");
+    while(1) { 
+
+        bzero(buff, 1024);
+        printf("Waiting for a reply...\n");
+
+        int recv_id = recvfrom(sock_id, buff, sizeof(buff),0 ,(struct sockaddr*)&store,&len_store);
+        if(recv_id == -1) {
+            printf("Cannot recieve the chat message:\n");
+            exit(1);
+        }
+
+        printf("MSG: %s\n", buff);
+        buff[strlen(buff)-1] = '\0';
+        if(strcmp(buff, "exit") == 0) {
+            printf("Quiting the chat session.\n");
+            break;
+        }
+
+        bzero(buff, 1024); 
+        printf("Enter a message to send:\n");
+        fgets(buff, 1024, stdin);
+
+        int send_id = sendto(sock_id, buff, strlen(buff), 0, (struct sockaddr*)&store, sizeof(store));
+        if(send_id == -1) {
+            printf("Cannot send the chat message!\n");
+            exit(1);
+        }
+        buff[strlen(buff)-1] = '\0';
+        if(strcmp(buff, "exit") == 0) {
+            printf("Quiting the chat session.\n");
+            break;
+        }
+    }
+    printf("---------------------------------------------\n"); 
+    printf("---------------------------------------------\n");
+}
+
+int main(int args, char *argv[]) {
+    if(args != 2) {
+        printf("Insufficient data!\n");
+        exit(1);
+    }
+
+    const int SERVER_PORT = atoi(argv[1]);
+    int CLIENT_PORT;
+
+    int sock_id = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_id == -1) {
+        printf("Cannot create socket!\n");
+        exit(1);
+    }
+
+    // Opening a UDP connection for the client at a predifiend port.
+    int udp_sockid = socket(AF_INET, SOCK_DGRAM, 0);
+    if(udp_sockid == -1) {
+        printf("Cannot create UDP socket!\n");
+        exit(1);
+    }
+
+    struct sockaddr_in self;
+    int self_bind = -1;
+
+    while(self_bind == -1) { // Loops till a valid udp socket is bind to the socket
+        printf("Enter the UDP port number:\n");
+        scanf("%d", &CLIENT_PORT);
+        getchar();
+        self.sin_addr.s_addr = INADDR_ANY;
+        self.sin_port = htons(CLIENT_PORT);
+        self.sin_family = AF_INET;
+
+        self_bind = bind(udp_sockid, (struct sockaddr*)&self, sizeof(self));
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(SERVER_PORT);
+  
+    int connect_id = connect(sock_id, (struct sockaddr*)&addr, sizeof(addr));
+    if(connect_id == -1) {
+        printf("Cannot send connection request!\n");
+        exit(1);
+    }
+
+    struct login self_details;
+    fd_set fd_arr;
+    FD_SET(sock_id, &fd_arr);
+    FD_SET(0, &fd_arr);
+    char buff[1024], reply[2];
+    bzero(buff, 1024);
+
+    struct peer peer_list[10]; // Peer list of individual client
+    for(int i=0; i<10; i++)
+        peer_list[i].peer_id = 0; 
+
+    bzero(buff, 1024);
+    int recv_id = recv(sock_id, buff, sizeof(buff), 0);
+    if(recv_id == -1) {
+        printf("Cannot recieve first server message:\n");
+        exit(1);
+    }
+    buff[strlen(buff)-1] = '\0';
+    puts(buff);
+    
+    fgets(reply, 2, stdin);
+    getchar();
+
+    int send_id = send(sock_id, reply, strlen(reply), 0);
+    if(send_id == -1) {
+        printf("Cannot send reply.\n");
+        exit(1);
+    }
+
+    printf("Enter login details:\n");
+    login:
+        bzero(self_details.username, 1024);
+        bzero(self_details.password, 1024);
+        printf("Enter username:\n");
+        fgets(self_details.username, 1024, stdin);
+        printf("Enter password:\n");
+        fgets(self_details.password, 1024, stdin);
+
+        self_details.username[strlen(self_details.username)-1] = '\0';
+        self_details.password[strlen(self_details.password)-1] = '\0';
+
+        printf("USRNAME: %s\nPASS: %s\n", self_details.username, self_details.password);
+        printf("Sending login details...\n");
+        send_id = send(sock_id, &self_details, sizeof(self_details), 0);
+        if(send_id == -1) {
+            printf("Cannot send login details:\n");
+            exit(1);
+        }
+        
+        bzero(buff, 1024);
+        recv_id = recv(sock_id, buff, sizeof(buff), 0);
+        if(recv_id == -1) {
+            printf("Cannot recieve greeting msg!\n");
+            exit(1);
+        }
+        
+        printf("FROM SERVER: %s\n", buff);
+
+    if(strcmp(buff, "Login Successful") == 0 || strcmp(buff, "Registration successful") == 0) {
+        printf("Sending the UDP socket info to server:\n");
+        int send_udpid = send(sock_id, &self, sizeof(self), 0);
+        if(send_udpid == -1) {
+            printf("Cannot send the UDP socket info to the client:\n");
+            exit(1);
+        }
+    }
+    else if(strcmp(buff, "Login Failed") == 0){
+        printf("Try again ...\n");
+        goto login;
+    }
+    else if(strcmp(buff, "Username taken.") == 0) {
+        goto login;
+    }
+    else if(strcmp(buff, "Already logged in.") == 0) {
+        printf("Multiple logins not supported.\n");
+        exit(0);
+    }
+
+    int maxfds = (sock_id > udp_sockid)?sock_id:udp_sockid;
+
+    while(1) {
+        FD_ZERO(&fd_arr);
+        FD_SET(sock_id, &fd_arr);
+        FD_SET(udp_sockid, &fd_arr);
+        FD_SET(0, &fd_arr);
+
+        int select_id = select(maxfds+1, &fd_arr, NULL, NULL, NULL);
+        if(select_id == -1) {
+            printf("Cannot select!\n");
+            exit(1);
+        }
+
+        if(FD_ISSET(sock_id, &fd_arr)) {
+            printf("Recieved a peer list update:\n");
+            struct peer new_user;
+            recv_id = recv(sock_id, peer_list, 10*sizeof(struct peer), 0);
+            if(recv_id == -1) {
+                printf("Cannot recieve new peer update!\n");
+                exit(1);
+            }
+            display_online_peers(peer_list);
+        }
+
+        if(FD_ISSET(udp_sockid, &fd_arr)) {
+            printf("A peer is trying to chat...\n");
+            chat_recv(udp_sockid);
+        }
+
+        if(FD_ISSET(0, &fd_arr)) {
+            bzero(buff, 100);
+            fgets(buff, 1024, stdin);
+            int input = isNumber(buff); 
+            if(input  >= 0) {
+                printf("Peer %d selected for communication.\n", input);
+                printf("Intiating Chat:\n");
+                chat(udp_sockid, peer_list[input]);
+            }
+            else if(strncmp(buff, "-s ", 3) == 0) {
+                printf("Sending the message to the server...\n");
+                if(strlen(buff) < 3)
+                    printf("Message too small!\n");
+                else {
+                    char *msg = buff + 3;
+                    int send_id = send(sock_id, msg, strlen(buff)-2, 0);
+                    if(send_id == -1) {
+                        printf("Cannot send message to the server!\n");
+                        exit(1);
+                    }
+
+                    if(strcmp(msg, "exit") == 0) {
+                        printf("Closing the peer..\n");
+                        exit(0);
+                    }
+                }
+            }
+        }
+    }
+
+    close(sock_id);
+    return 0;
+}    
---
+++ server.c
@@ -0,0 +1,328 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+struct login {
+    int online;
+    char username[1024];
+    char password[1024];
+};
+
+struct peer {
+    int peer_id; // !0 -> online, 0 - > offline
+    char ip[20];
+    int port;
+};
+
+void showDatabase(struct login database[], int database_counter) {
+    // Displays the database of username and password for different clients.
+
+    printf("---------------------------------------------\n");
+    if(database_counter == -1) {
+        printf("Database is Empty!\n");
+        return;
+    }
+    printf("DATABASE:\n\n");
+    for(int i=0; i<=database_counter; i++)
+        printf("%d. USERNAME: %s PASSWORD: %s\n",i+1, database[i].username, database[i].password);
+    printf("---------------------------------------------\n");
+}
+
+void usersOnlineDB(struct login database[], int database_counter) {
+    // Shows the online users in the databse.
+
+    printf("---------------------------------------------\n");
+    if(database_counter == -1) {
+        printf("Database is Empty!\n");
+        return;
+    }
+    printf("ONLINE USERS IN DATABASE:\n\n");
+    for(int i=0; i<database_counter; i++) {
+        if(database[i].online == 1)
+            printf("%d. USERNAME: %s PASSWORD: %s\n",i+1, database[i].username, database[i].password);
+    }
+    printf("---------------------------------------------\n");
+
+}
+
+
+int userNameTaken(struct login user, struct login database[], int database_counter) {
+    // Determines if the username is already taken or not
+
+    for(int i=0; i<=database_counter; i++)
+        if(strcmp(database[i].username, user.username) == 0)
+            return 1;
+
+    return 0;
+}
+
+
+void showOnlineClients(struct peer online_clients[]) {
+    // Shows the list of all online clients
+
+    printf("---------------------------------------------\n");
+    printf("CLIENTS ONLINE:\n\n");
+    int count = 1;
+    for(int i=0; i<10; i++)
+        if(online_clients[i].peer_id != 0)
+            printf("%d. TCP_PEER_ID: %d. IP: %s PORT: %d\n",count++, online_clients[i].peer_id, online_clients[i].ip, online_clients[i].port);
+    printf("---------------------------------------------\n");
+        
+}
+
+
+int authenticate_login(struct login user, struct login *database, int database_counter) {
+    // Authenticates the login details provided by the user.
+    
+    for(int i=0; i<=database_counter; i++) 
+        if(strcmp(database[i].username, user.username) == 0 && strcmp(database[i].password, user.password) == 0) {
+            if(database[i].online == 0) {
+                database[i].online = 1;
+                return 1;
+            }
+            else 
+                return 0;
+        }
+    return -1;
+}
+
+void broadcast(struct peer online_clients[]) {
+    // Broadcasts the new user's ip and port to all online users
+
+    printf("Initiating Broadcasting:\n");
+    int send_id;
+    for(int i=0; i<10; i++) {
+        if(online_clients[i].peer_id != 0) {
+            send_id = send(online_clients[i].peer_id, online_clients, 10*sizeof(struct peer), 0);
+            if(send_id == -1) {
+                printf("Problem in broadcast!\n");
+                exit(1);
+            } 
+        }
+    }
+}
+
+int main(int args, char *argv[]) {
+
+    if(args != 2) {
+        printf("Server port number needed!\n");
+        exit(1);
+    }
+
+    const int SERVER_PORT = atoi(argv[1]);
+
+    int sock_id = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_id == -1) {
+        printf("Cannot create socket!\n");
+        exit(1);
+    }
+
+    struct sockaddr_in addr, store; int len_store = sizeof(store);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(SERVER_PORT);
+
+    int bind_id = bind(sock_id, (struct sockaddr*)&addr, sizeof(addr));
+    if(bind_id == -1) {
+        printf("Cannot bind!\n");
+        exit(1);
+    }
+
+    int listen_id = listen(sock_id, 10);
+    if(listen_id == -1) {
+        printf("Cannot listen!\n");
+        exit(1);
+    }
+    
+    int maxfds = sock_id;
+    char buff[1024];
+    fd_set fd_arr;
+
+    struct peer online_clients[10]; // Maximum 10 peers can be online
+    for(int i=0; i<10; i++)
+        online_clients[i].peer_id = 0;  // Initially, all the peers are offline.
+
+    struct login database[100]; int database_counter = -1;   // Record the details of all peer login
+
+    printf("Waiting for connections..\n");
+
+    while(1) {
+
+        FD_ZERO(&fd_arr);
+        FD_SET(sock_id, &fd_arr);
+        FD_SET(0, &fd_arr);
+
+        for(int i=0; i<10; i++) {
+            if(online_clients[i].peer_id > 0)
+                FD_SET(online_clients[i].peer_id, &fd_arr);
+            if(online_clients[i].peer_id > maxfds)
+                maxfds = online_clients[i].peer_id;
+        }
+
+        int select_id = select(maxfds+1, &fd_arr, NULL, NULL, NULL);
+        if(select_id == -1) {
+            printf("Cannot select!\n");
+            exit(1);
+        }
+
+        if(FD_ISSET(sock_id, &fd_arr)) {
+            int isNewClient = 0;
+
+            printf("A client is trying to connect. Check the login details.\n");
+            int client_id = accept(sock_id, (struct sockaddr*)&store, &len_store);
+            if(client_id == -1) {
+                printf("Cannot accept!\n");
+                exit(1);
+            }
+
+            int send_id = send(client_id, "New User? y or n: ", strlen("New User? y or n: "), 0);
+            if(send_id == -1) {
+                printf("Cannot send 'New User' confirmation message!\n");
+                exit(1);
+            }
+            
+            bzero(buff, 1024);
+            int recv_id = recv(client_id, buff, sizeof(buff), 0);
+            if(recv_id == -1) {
+                printf("Cannot recieve 'New user' reply!\n");
+                exit(1);
+            }
+
+            printf("FROM CLIENT: %s\n", buff);;
+            if(strcmp(buff, "y") == 0)
+                isNewClient = 1;
+            else if(strcmp(buff, "n") != 0) {
+                printf("Wrong input from client!\n");
+                continue;
+            }
+
+            struct peer new_user; // New user storage strucutre.
+            struct login new_user_login;
+
+            recv_id = recv(client_id, &new_user_login, sizeof(new_user_login), 0);
+            if(recv_id == -1) {
+                printf("Cannot recieve login details!\n");
+                exit(1);
+            }
+
+            if(isNewClient) {
+                // Need to add it to all_clients_ever, online client and assign a user name.
+                // Add to the all_clients list (historical record of clients)
+
+                if(userNameTaken(new_user_login, database, database_counter) == 1) {
+                    printf("Used Username entered by the client.\n");
+                    send_id = send(client_id, "Username taken.", strlen("Username taken."), 0);
+                    if(send_id == 0) {
+                        printf("Cannot send username taken message!\n");
+                        exit(1);
+                    }
+                }   
+                else {
+                    printf("Registering the username and password to the database..\n");
+                    database_counter++;
+                    strcpy(database[database_counter].username, new_user_login.username);
+                    strcpy(database[database_counter].password, new_user_login.password);
+                    database[database_counter].online = 1;
+
+                    int send_id = send(client_id, "Registration successful", strlen("Registration successful"), 0);
+                    if(send_id == -1) {
+                        printf("Cannot send successful registration msg\n");
+                        exit(1);
+                    }
+                }
+            }
+            else { // User had a connection with the server in the past.
+                int auth_stat = authenticate_login(new_user_login, database, database_counter);
+                if(auth_stat == 1) {
+                    printf("Client login succesfull! Sending the greeting msg..\n");
+                    int send_id = send(client_id, "Login Successful", strlen("Login Successful"), 0);
+                    if(send_id == -1) {
+                        printf("Cannot send the greeting msg!\n");
+                        exit(1);
+                    }
+
+                    usersOnlineDB(database, database_counter);
+
+                }
+                else if(auth_stat == 0) {
+                    printf("User already loged in!\n");
+                    send_id = send(client_id, "Already logged in.", strlen("Already logged in."), 0);
+                    if(send_id == -1) {
+                        printf("Cannot send already logged in message!\n");
+                        exit(1);
+                    }
+                }
+                else {  // user login failed
+                    printf("Authentication failed. Sending the msg..\n");
+                    int send_id = send(client_id, "Login Failed", strlen("Login Failed"), 0);
+                    if(send_id == -1) {
+                        printf("Cannot send the failed login msg!\n");
+                        exit(1);
+                    }
+                    close(client_id);
+                    continue;   
+                }
+            }
+
+            // Continuation means, either a new user or successful login.
+            struct sockaddr_in udp_id; // To store the UPD socket details of the client.
+            int recv_udpid = recv(client_id, &udp_id, sizeof(udp_id), 0);
+            if(recv_udpid == -1) {
+                printf("Cannot recieve the UDP socket details of the client!\n");
+                exit(1);
+            }
+
+            printf("Recieved UDP info for the client %d.\n", client_id);
+            new_user.peer_id = client_id;
+            strcpy(new_user.ip, inet_ntoa(udp_id.sin_addr));
+            new_user.port = ntohs(udp_id.sin_port);
+
+            for(int i=0; i<10; i++) { // Add the client list to online client_list.
+                if(online_clients[i].peer_id == 0) {
+                    online_clients[i] = new_user;
+                    break;
+                }
+            }
+          
+            broadcast(online_clients);
+
+            showDatabase(database, database_counter);
+            showOnlineClients(online_clients);
+        }
+
+        for(int i=0; i<10; i++) {
+            if(online_clients[i].peer_id != 0 && FD_ISSET(online_clients[i].peer_id, &fd_arr)) {
+                printf("Recieving a message from the client:\n");
+                bzero(buff, 1024);
+                int recv_id = recv(online_clients[i].peer_id, buff, sizeof(buff), 0);
+                if(recv_id == -1) {
+                    printf("Cannot recieve message from client!\n");
+                    exit(1);
+                }
+                //buff[strlen(buff)-1] = '\0';
+                printf("FROM CLIENT: %s\n", buff);
+
+                if(strcmp(buff, "exit") == 0) {
+                    printf("Connection close request recieve. Processing...\n");
+                    int temp = online_clients[i].peer_id;
+                    online_clients[i].peer_id = 0;
+                    close(temp);
+                    
+                    broadcast(online_clients); // Broadcast the updated client list.
+
+                    showDatabase(database, database_counter);
+                    showOnlineClients(online_clients);
+                }
+            }
+        }
+    }
+
+
+    close(sock_id);
+    return 0;
+}
